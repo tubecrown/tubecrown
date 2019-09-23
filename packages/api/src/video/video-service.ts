f50b023249f9406b82a32fccd8cd927cec79f8fb
Bao D <baocancode@gmail.com>
2019-09-24 00:02:24 +1000

    dev: update youtube search :recycle:

--- packages/api/src/video/video-service.ts
+++ packages/api/src/video/video-service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common'
 import { VideoDto } from '@tubecrown/core/lib/video'
 import { YouTube } from '@tubecrown/youtube'
-import { Video } from '@tubecrown/youtube/lib/exports'
 import { DateTime } from 'luxon'
 import { ConfigService, HtmlService } from '../common'
 
@@ -15,15 +14,15 @@ export class VideoService {
 
   async search (): Promise<VideoDto[]> {
     const currentDate = DateTime.utc().set({ hour: 0, minute: 0, second: 0, millisecond: 0 })
-    const videoSearchReponse = await this.youTubeApiClient.searchVideos({
+    const searchVideoResponse = await this.youTubeApiClient.searchVideos({
       startDate: currentDate.minus({ day: 1 }).toISO(),
       endDate: currentDate.toISO(),
       regionCode: 'US',
       maxResults: 50,
     })
-    const videoIds: string[] = videoSearchReponse.items.map((searchVideoResult) => searchVideoResult.id.videoId)
-    const videoById: Record<string, Video> = await this.youTubeApiClient.getVideoDetails(videoIds)
-    return videoSearchReponse.items.map((searchVideoResult) => {
+    const videoIds: string[] = searchVideoResponse.items.map((searchVideoResult) => searchVideoResult.id.videoId)
+    const videoById: Record<string, YouTube.Video> = await this.youTubeApiClient.getVideoDetails(videoIds)
+    return searchVideoResponse.items.map((searchVideoResult) => {
       const { title, publishedAt, thumbnails, channelId, channelTitle } = searchVideoResult.snippet
       const { contentDetails, statistics } = videoById[searchVideoResult.id.videoId]
       return new VideoDto({
