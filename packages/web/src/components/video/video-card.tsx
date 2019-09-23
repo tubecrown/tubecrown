da783d5c6f6317764232ca811fc7a28b46cfb181
Chau Nguyen <nmchau87@gmail.com>
2019-09-23 22:21:57 +0900

    feat: format duration :crown: (#2)

--- packages/web/src/components/video/video-card.tsx
+++ packages/web/src/components/video/video-card.tsx
@@ -1,6 +1,6 @@
 import { VideoDto } from '@tubecrown/core/lib/video'
 import { Component, Prop, Vue } from 'vue-property-decorator'
-import { formatFromNow } from '../../utils/datetimes'
+import { formatDuration, formatFromNow } from '../../utils/datetimes'
 import { formatShortNumber } from '../../utils/numbers'
 
 export interface VideoClickEvent {
@@ -19,10 +19,14 @@ export class VideoCard extends Vue {
   }
 
   render () {
-    const { id, titleHtml, publishedAt, thumbnail, channelTitle, viewCount } = this.video
+    const { id, titleHtml, publishedAt, thumbnail, channelTitle, viewCount, duration } = this.video
     return (
       <nuxt-link to={`/v/${id}`} event='' nativeOn={{ click: this.handleClick }} class='video-card'>
-        <v-img src={thumbnail} aspect-ratio={1.5}/>
+        <v-img src={thumbnail} aspect-ratio={1.5}>
+          <div class='video-card__duration white--text px-1'>
+            {formatDuration(duration)}
+          </div>
+        </v-img>
         <div class='video-card__title text__two-lines font-weight-medium mt-2' domProps={{ innerHTML: titleHtml }}/>
         <div class='video-card__subtitle text__two-lines mt-1'>
           {channelTitle}
